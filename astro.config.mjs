23a87df1c2578805fe5f8dbe7eadb4350ea1ec25
Brian Shirai <brixen@gmail.com>
2025-10-31 09:56:46 -0700

    Update GitHub Pages workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import solidJs from '@astrojs/solid-js';
-
 import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'My Docs',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-        sidebar: [
-            {
-                label: 'Guides',
-                items: [
-                    // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
+  site: 'https://vivarium-ai.com',
+  base: '/',   // omit or leave as '/'
+
+  integrations: [starlight({
+    title: 'My Docs',
+    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+    sidebar: [
+      {
+        label: 'Guides',
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: 'Example Guide', slug: 'guides/example' },
         ],
-        }), solidJs(), markdoc()],
+      },
+      {
+        label: 'Reference',
+        autogenerate: { directory: 'reference' },
+      },
+    ],
+  }), solidJs(), markdoc()],
 });
