6840d29be82ec804d883b0f92c9a46a2eb140352
Brian Shirai <brixen@gmail.com>
2025-11-02 15:18:24 -0800

    Start building up structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
 import solidJs from '@astrojs/solid-js';
 import markdoc from '@astrojs/markdoc';
 
 export default defineConfig({
   output: "static",
   site: 'https://vivarium-ai.com',
-  base: '/',   // omit or leave as '/'
+  base: '/',
 
-  integrations: [starlight({
-    title: "Vivarium AI Docs",
-    social: [{ icon: 'github', label: 'Vivarium', href: 'https://github.com/vivarium-ai' }],
-    sidebar: [
-      {
-        label: "Tutorials",
-        autogenerate: { directory: "docs/tutorials" },
-      },
-      {
-        label: "How-tos",
-        autogenerate: { directory: "docs/howtos" },
-      },
-      {
-        label: "Explanations",
-        autogenerate: { directory: "docs/explanations" },
-      },
-      {
-        label: "Reference",
-        autogenerate: { directory: "docs/reference" },
-      },
-    ],
-  }), solidJs(), markdoc()],
+  integrations: [solidJs(), markdoc()],
 });
