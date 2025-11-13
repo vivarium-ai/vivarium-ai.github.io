c063f12f1692780f2a3aa12627b6ed95ef471c7f
Brian Shirai <brixen@gmail.com>
2025-11-12 20:39:37 -0800

    Improve RSS feed.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,14 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 import { getBlogList } from '@/lib/blog';
 
 export async function GET() {
-  // const posts = await getCollection('blog', (p) => p.data.published !== false);
   const posts = await getBlogList();
 
   return rss({
+    xmlns: {
+      atom: 'http://www.w3.org/2005/Atom',
+    },
+    customData: `<atom:link href="${import.meta.env.SITE}/rss.xml" rel="self" type="application/rss+xml" />`,
     title: 'Vivarium AI Blog',
     description: 'An experimental platform for real AI.',
     site: import.meta.env.SITE,
